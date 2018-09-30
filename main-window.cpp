59edc78614d356628592b329de7a7ae319e720bf
Jan Horacek <me@apophis.cz>
2018-09-30 13:21:55 +0200

    Load & store config.

--- main-window.cpp
+++ main-window.cpp
@@ -2,9 +2,30 @@
 #include "ui_main-window.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-	QMainWindow(parent) {
+	QMainWindow(parent), s(_CONFIG_FN) {
 	ui.setupUi(this);
+
+	widget_set_color(*(ui.l_xn), Qt::red);
+	widget_set_color(*(ui.l_dcc), Qt::gray);
+	widget_set_color(*(ui.l_wsm), Qt::red);
+	widget_set_color(*(ui.l_wsm_alive), Qt::gray);
 }
 
 MainWindow::~MainWindow() {
+	try {
+		s.save(_CONFIG_FN);
+	}
+	catch (...) {
+		// No exceptions in destructor!
+	}
 }
+
+///////////////////////////////////////////////////////////////////////////////
+
+void MainWindow::widget_set_color(QWidget& widget, const QColor color) {
+	QPalette palette = widget.palette();
+	palette.setColor(QPalette::WindowText, color);
+	widget.setPalette(palette);
+}
+
+///////////////////////////////////////////////////////////////////////////////
--- main-window.h
+++ main-window.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 
 #include "ui_main-window.h"
+#include "lib/xn/xn.h"
+#include "settings.h"
+
+const QString _CONFIG_FN = "config.ini";
 
 class MainWindow : public QMainWindow {
 	Q_OBJECT
@@ -14,6 +18,10 @@ public:
 
 private:
 	Ui::MainWindow ui;
+	XpressNet xn;
+	Settings s;
+
+	void widget_set_color(QWidget&, const QColor);
 };
 
 #endif // MAINWINDOW_H
