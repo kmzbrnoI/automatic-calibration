6ce136e87c5b2e1630eb59e0c228f3b654d0d596
Jan Horacek <me@apophis.cz>
2018-10-09 15:55:51 +0200

    CalibStep: implement basic functionality.

--- calib-man.cpp
+++ calib-man.cpp
@@ -1,16 +1,79 @@
+#include <cmath>
+
 #include "calib-man.h"
 
 namespace Cm {
 
-CalibStep::CalibStep(QObject *parent) : QObject(parent) {}
+CalibStep::CalibStep(Xn::XpressNet& xn, Pm::PowerToSpeedMap& pm, QObject *parent)
+	: QObject(parent), m_xn(xn), m_pm(pm) {
+	t_sp_adapt.setSingleShot(true);
+	QObject::connect(&t_sp_adapt, SIGNAL(timeout()), this, SLOT(t_sp_adapt_tick()));
+}
 
-void CalibStep::calibrate(unsigned loco_addr, unsigned step, double speed, double epsilon) {
+void CalibStep::calibrate(const unsigned loco_addr, const unsigned step,
+                          const double speed, const double epsilon) {
+	m_loco_addr = loco_addr;
+	m_step = step;
+	m_target_speed = speed;
+	m_epsilon = epsilon;
+
+	m_last_power = m_pm.steps(m_target_speed);
+
+	m_xn.PomWriteCv(
+		Xn::LocoAddr(m_loco_addr),
+		_CV_START - 1 + m_step,
+		m_last_power,
+		std::make_unique<Xn::XnCb>(&xns_pom_ok, this),
+		std::make_unique<Xn::XnCb>(&xns_pom_err, this)
+	);
 }
 
 void CalibStep::wsm_lt_read(double speed, double diffusion) {
+	if (diffusion > _MAX_DIFFUSION) {
+		diffusion_error();
+		return;
+	}
+	if (speed == 0) {
+		loco_stopped();
+		return;
+	}
+
+	if (std::abs(speed-m_target_speed) < m_epsilon) {
+		done();
+		return;
+	}
+
+	m_pm.addOrUpdate(m_last_power, speed);
+	m_last_power = m_pm.steps(m_target_speed);
+
+	m_xn.PomWriteCv(
+		Xn::LocoAddr(m_loco_addr),
+		_CV_START - 1 + m_step,
+		m_last_power,
+		std::make_unique<Xn::XnCb>(&xns_pom_ok, this),
+		std::make_unique<Xn::XnCb>(&xns_pom_err, this)
+	);
 }
 
 void CalibStep::t_sp_adapt_tick() {
+	// TODO: call WSM long time measuring init
+}
+
+void CalibStep::xns_pom_ok(void* s, void* d) { static_cast<CalibStep*>(d)->xn_pom_ok(s, d); }
+void CalibStep::xns_pom_err(void* s, void* d) { static_cast<CalibStep*>(d)->xn_pom_err(s, d); }
+
+void CalibStep::xn_pom_ok(void* source, void* data) {
+	(void)source;
+	(void)data;
+
+	// Insert 'waiting of mark' here when neccessarry
+	t_sp_adapt.start(_SP_ADAPT_TIMEOUT);
+}
+
+void CalibStep::xn_pom_err(void* source, void* data) {
+	(void)source;
+	(void)data;
+	xn_error();
 }
 
 }//end namespace
--- calib-man.h
+++ calib-man.h
@@ -4,33 +4,51 @@
 #include <QObject>
 #include <QTimer>
 
+#include "lib/xn/xn.h"
+#include "lib/wsm/measure-car.h"
+#include "power-map.h"
+
 namespace Cm {
 
 const double _DEFAULT_EPSILON = 0.5; // +- 0.7 kmph
+const unsigned _SP_ADAPT_TIMEOUT = 2000; // 2 s
+const double _MAX_DIFFUSION = 3; // 3 kmph
+const unsigned _CV_START = 67; // cv 67 = step 1
 
 class CalibStep : public QObject {
 	Q_OBJECT
 
 public:
-	CalibStep(QObject *parent = nullptr);
-	void calibrate(unsigned loco_addr, unsigned step,
-	               double speed, double epsilon = _DEFAULT_EPSILON);
+	CalibStep(Xn::XpressNet& xn, Pm::PowerToSpeedMap& pm, QObject *parent = nullptr);
+	void calibrate(const unsigned loco_addr, const unsigned step,
+	               const double speed, const double epsilon = _DEFAULT_EPSILON);
 
 private:
+	Xn::XpressNet& m_xn;
+	Pm::PowerToSpeedMap& m_pm;
+
 	unsigned m_loco_addr;
-	unsigned step;
+	unsigned m_step;
 	double m_target_speed;
 	double m_epsilon;
 	QTimer t_sp_adapt;
+	unsigned m_last_power;
+
+	static void xns_pom_ok(void*, void*);
+	static void xns_pom_err(void*, void*);
+	void xn_pom_ok(void*, void*);
+	void xn_pom_err(void*, void*);
 
 private slots:
 	void wsm_lt_read(double speed, double diffusion);
 	void t_sp_adapt_tick();
 
+
 signals:
 	void diffusion_error();
 	void loco_stopped();
 	void done();
+	void xn_error();
 };
 
 }//end namespace
