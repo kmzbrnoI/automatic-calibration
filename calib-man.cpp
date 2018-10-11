797f1e18caf9d0a050c9c1af92c3da002401da7a
Jan Horacek <me@apophis.cz>
2018-10-11 16:25:33 +0200

    Cm: implement algorithm of getting next step.

--- calib-man.cpp
+++ calib-man.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+
 #include "calib-man.h"
 
 namespace Cm {
@@ -20,5 +22,48 @@ void CalibMan::csStepPowerChanged(unsigned step, unsigned power) {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+
+std::unique_ptr<unsigned> CalibMan::nextStep() {
+	std::vector<unsigned> used_steps; // map of indexes of steps
+	int last = -1;
+	for(size_t i = 0; i < Xn::_STEPS_CNT; i++)
+		if (nullptr != m_ssm.map[i] && static_cast<int>(*m_ssm.map[i]) != last) {
+			used_steps.push_back(i);
+			last = *m_ssm.map[i]; // avoid duplicate speds
+		}
+
+	if (used_steps.size() == 0) // no available steps
+		return nullptr;
+
+	// Check if min step calibred
+	if (state[used_steps[0]] == StepState::Uncalibred)
+		return std::make_unique<unsigned>(used_steps[0]);
+
+	// Check if max step calibred
+	if (state[used_steps[used_steps.size()-1]] == StepState::Uncalibred)
+		return std::make_unique<unsigned>(used_steps[used_steps.size()-1]);
+
+	// Binary search next step; interval=[left, right)
+	return std::move(nextStepBin(used_steps, 0, used_steps.size()));
+}
+
+std::unique_ptr<unsigned> CalibMan::nextStepBin(const std::vector<unsigned>& used_steps,
+                                                const size_t left, const size_t right) {
+	size_t middle = ((right-left) / 2) + left;
+	if (state[used_steps[middle]] == StepState::Uncalibred)
+		return std::make_unique<unsigned>(middle);
+
+	std::unique_ptr<unsigned> res;
+
+	res = std::move(nextStepBin(used_steps, left, middle));
+	if (nullptr != res)
+		return res;
+
+	res = std::move(nextStepBin(used_steps, middle, right));
+	if (nullptr != res)
+		return res;
+
+	return nullptr;
+}
 
 }//end namespace
--- calib-man.h
+++ calib-man.h
@@ -2,6 +2,8 @@
 #define _CALIB_MAN_H_
 
 #include <QObject>
+#include <memory>
+#include <vector>
 
 #include "lib/xn/xn.h"
 #include "lib/wsm/wsm.h"
@@ -25,9 +27,17 @@ public:
 	CalibMan(Xn::XpressNet& xn, Pm::PowerToSpeedMap& pm, Wsm::Wsm& wsm,
 	         Ssm::StepsToSpeedMap& ssm, QObject *parent = nullptr);
 
+	void calibrateAll();
+
 private:
 	Ssm::StepsToSpeedMap& m_ssm;
 	Xn::XpressNet& m_xn;
+	StepState state[Xn::_STEPS_CNT]; // step index used as index
+
+	std::unique_ptr<unsigned> nextStep(); // returns step index
+	void calibrateNextStep();
+	std::unique_ptr<unsigned> nextStepBin(const std::vector<unsigned>& used_steps,
+	                                      const size_t left, const size_t right);
 
 private slots:
 	void csDone();
