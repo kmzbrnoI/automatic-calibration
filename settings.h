4db066a0fe860ae9e7998ec4227b41b11e5ec757
Jan Horacek <me@apophis.cz>
2018-10-19 12:09:11 +0200

    Nicer std::map initialization.

--- settings.h
+++ settings.h
@@ -8,22 +8,19 @@
 /* This file defines global settings of the application */
 
 const std::map<QString, std::map<QString, QVariant>> _DEFAULTS {
-	std::pair<QString, std::map<QString, QVariant>>("WSM",
-	{
-		std::pair<QString, QVariant>("scale", 120),
-		std::pair<QString, QVariant>("wheelDiameter", 8.0),
-	}),
-	std::pair<QString, std::map<QString, QVariant>>("XN",
-	{
-		std::pair<QString, QVariant>("baudrate", 19200),
-		std::pair<QString, QVariant>("flowcontrol", 1),
-		std::pair<QString, QVariant>("loglevel", 1),
-		std::pair<QString, QVariant>("port", "/dev/ttyUSB0"),
-	}),
-	std::pair<QString, std::map<QString, QVariant>>("Speed",
-	{
-		std::pair<QString, QVariant>("file", "speed.csv"),
-	}),
+	{"WSM", {
+		{"scale", 120},
+		{"wheelDiameter", 8.0},
+	}},
+	{"XN", {
+		{"baudrate", 19200},
+		{"flowcontrol", 1},
+		{"loglevel", 1},
+		{"port", "/dev/ttyUSB0"},
+	}},
+	{"Speed", {
+		{"file", "speed.csv"},
+	}},
 };
 
 class Settings {
